d74c389fd8f27a6d3d08c2554b3786166fe8c964
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2021-01-12 20:51:00 -0800

    fixed issue where data wasn't being cached

--- helpers/parseResBody.js
+++ helpers/parseResBody.js
@@ -2,32 +2,33 @@ const { saveToCache } = require('../models/saveToCache.js');
 const { deleteFromCache } = require('../models/deleteFromCache.js');
 
 const parseResBody = (req, res, next) => {
-  let oldWrite = res.write,
-  oldEnd = res.end;
-  let chunks = [];
-  let resBody;
+    let oldWrite = res.write,
+    oldEnd = res.end;
+    let chunks = [];
+    let resBody;
+    let productId = req.params.product_id;
+    
+    res.write = function (chunk) {
+        chunks.push(chunk);
+        return oldWrite.apply(res, arguments);
+    };
 
-  res.write = function (chunk) {
-      chunks.push(chunk);
-      return oldWrite.apply(res, arguments);
-  };
-
-  res.end = function (chunk) {
-      if (chunk){
-          chunks.push(chunk);
-      }
-      if (typeof chunks[0] !== 'string' && chunks[0].toString('utf8') !== 'OK' && res.statusCode !== 404 && res.statusCode !== 500) {
+    res.end = function (chunk) {
+        if (chunk){
+            chunks.push(chunk);
+        }
+        if (typeof chunks[0] !== 'string' && chunks[0].toString('utf8') !== 'OK' && res.statusCode !== 404 && res.statusCode !== 500) {
         resBody = JSON.parse(Buffer.concat(chunks));
         if (resBody.reviews) {
-          saveToCache(req.params.product_id, resBody);
+            saveToCache(productId, resBody);
         }
 
         if (resBody.rows) {
-          deleteFromCache(resBody.rows[0].product_id, res);
+            deleteFromCache(resBody.rows[0].product_id, res);
         }
-      }
-      oldEnd.apply(res, arguments);
-  };
+        }
+        oldEnd.apply(res, arguments);
+    };
 
   next();
 };
